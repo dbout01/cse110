e253a0f23a3649b32861967766aca113a9763ccb
Dylan Butterfield <but20002@byui.edu>
2022-11-16 14:59:37 -0700

    changes

--- Week 10/prove.py
+++ Week 10/prove.py
@@ -1,45 +1,66 @@
-shopping_list = []
+print()
+print('Welcome to the Shopping Cart Program!')
+menu = ['Add item', 'View Cart', 'Remove an Item', 'View Total', 'Quit']
+
+cart = []
 
 prices = []
 
-number = ''
 
-while number != '5':
+while True:
+    for i, option in enumerate(menu):
+        print(f'{i +1} {option}')
+    
+    choice=  0
+
+    try:
+        choice = int(input('Please enter an action: '))
+    except:
+        print('please enter only a number')
+        continue
+
+#ADD ITEM
+    if choice == 1:
+        new_item = input('What would you like to add?: ') #you can add nested while
+        cart.append(new_item) #you can add nest menu
+        price = float(input(f'What is the price of {new_item}?: '))
+        prices.append(price)
         print()
-        print("""Welcome to the Shopping Cart Program!
+        print(f'{new_item} added!')
+        print()
+        pass
 
-        Please select one of the following:
-        1. Add item
-        2. View cart
-        3. Remove item
-        4. Compute total
-        5. Quit
-        """)
+#CONTENTS
+    elif choice == 2:
+        print()
+        print("The contents of the shopping cart are: ")
+        for i in cart:
+            print(i)
+        pass
 
-        selection = input("Please enter an action: ")
-    #ADD
-        if selection == "1":
-            item = input("What item would you like to add?: ")
-            shopping_list.append(item)
-            print(item + " has been added to the cart.")
-            price = float(input(f'What is the price of {item}?: '))
-            prices.append(price)
+#REMOVE
+    elif choice == 3:
+        old_item = input('What is the name of the item to remove?: ')
+        try:
+            choice = int(input('Type the number you would like: '))
+        except:
+            print('please enter only a number')
+        continue
+    #add for loop
+    #add number question
 
-    #REMOVE
-        elif selection == "3":
-            kill = int(input('Which item would you like to remove?: '))
+#TOTAL
+    elif choice == 4:
+        total = 0
+        for item_price in prices:
+            total = item_price + total
+            print(f'The total price of the items in the shopping cart is {total:.2f}')
+        pass
 
-            try:
-                choice = int(input('Type the number you would like: '))
-            except:
-                print('please enter only a number')
-            continue
+#QUIT
+    elif choice == 5:
+         print('Thank you. Goodbye.')
+         break
 
-
-
-    #TOTAL  
-        elif selection == '4':
-            total = 0
-            for item_price in prices:
-                total = item_price + total
-                print(f'The total price of the items in the shopping cart is {total:.2f}')
+    else:
+        print('Please choose a number between 1 and {len(menu)}')
